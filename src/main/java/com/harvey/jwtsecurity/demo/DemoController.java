026f9c2cd66cb30e1fdd04a1d9261e5f35cc5d82
harvey-jean <ihevea@gmail.com>
2024-11-04 20:05:58 +0100

    Initial commit

---
+++ src/main/java/com/harvey/jwtsecurity/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.harvey.jwtsecurity.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> demo() {
+        return ResponseEntity.ok("Hello World from secured endpoint");
+    }
+}
